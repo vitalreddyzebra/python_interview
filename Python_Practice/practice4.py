1ce54f06096cc6f94229a7118a5bb3e81204b7ae
vitalreddy <vitalreddy.c@gmail.com>
2023-06-08 02:14:19 -0700

    adding practice4 file for try and catch for file

--- Python_Practice/practice4.py
+++ Python_Practice/practice4.py
@@ -5,3 +5,24 @@ def my_function():
     finally:
         print("world")
 my_function()
+
+########################################################################################
+
+numbers=[1,2,3]
+
+try:
+    value=numbers[0]
+except IndexError as e:
+    print(f'An error occurred:{e}')
+###########################################################################################
+
+def divide(a,b):
+    try:
+        result=a/b
+    except ZeroDivisionError:
+        print("Opps! Division by Zero is not allowed")
+    else:
+        print(f"The result is : {result}")
+    finally:
+        print("The Divided function has been executed")
+divide(10,0)        
